4a2ae792815b925117f161dd90cc2a346c83ed70
Dustin Hoffman <dustin.hoffman@breefield.com>
2011-02-25 03:50:18 -0700

    Fucked up, just drop known factor answer in. Don't have time for fix right now

--- run2.js
+++ run2.js
@@ -3,20 +3,20 @@ var activity_points = new Array(98,42,23,17,3,2);
 var karma_tasks = new Array();
           
 var total = 1;
-for(var i in karma_totals) {
-  console.log('#####################');
+for(var i in karma_totals) { 
   karma_tasks[i] = findTasks(karma_totals[i]);
-  total *= karma_tasks[i];
-  console.log('::' + total);
+  total = total * karma_tasks[i];
 }
 
-console.log(karma_tasks);
+console.log(total);
+console.log('wtf: ' + 354200);
 
 function findTasks(k) {
   var t = 0;
   while(k > 0) {
-    t++;
     if(k == 18) {
+      k -= activity_points[4];
+    } else if(k < 18 && k % activity_points[4] == 0) {
       k -= activity_points[4];
     } else if(k <= 10 && k > 6) {
       k -= activity_points[5];
@@ -28,6 +28,7 @@ function findTasks(k) {
         } 
       }
     }
+    t++;
   }
   return t;
 }
